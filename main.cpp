b403d16f3f07359d51ebc6b3e880cf36c8595d0c
최기헌 <choigiheon@choegiheon-ui-MacBookPro.local>
2022-12-15 04:17:53 +0900

    First commit

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+namespace MyArray {
+
+    class Array;
+    class Int;
+
+    class Array {
+        friend class Int;
+
+        int dim;
+        int *size;
+
+        struct data {
+            int* location;
+            int num;
+        };
+
+        data* start;
+
+    public:
+        Array(int dim, int *size) : dim(dim), size(size) {
+
+            int capacity = size[0];
+            for(int i = 1; i < dim; i++) {
+                capacity *= size[i];
+            }
+
+            start = new data[capacity];
+        }
+
+        Int operator[](const int index);
+    };
+
+    class Int {
+        int depth;
+
+        Array* arr;
+        data* target;
+
+    public:
+        Int(int index, int depth, Array* arr) : depth(depth), arr(arr) {
+        }
+
+        Int operator[](const int index) {
+            return Int(index, depth + 1, arr);
+        }
+
+        operator int() {
+            return target->num;
+        }
+    };
+
+    Int Array::operator[](const int index) {
+        Int temp = Int(index, 1,this);
+    }
+}
+
+int main() {
+
+}
